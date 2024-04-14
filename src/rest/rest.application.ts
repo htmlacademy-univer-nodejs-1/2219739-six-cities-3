5b3dbb03a1e57b19822edb3b54f1e500c887ef1d
malinainsugar <super.a03-a2013@yandex.ru>
2024-04-15 00:06:51 +0500

    добавление MongoDB

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -2,17 +2,35 @@ import { Logger } from '../shared/limbs/logger/index.js';
 import { Config, ConfigSchema } from '../shared/limbs/config/index.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../shared/types/index.js';
+import { DatabaseClient } from '../shared/limbs/database-client/index.js';
+import { getMongoURI } from '../shared/helpers/index.js';
 
 @injectable()
 export class Application {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<ConfigSchema>,
+    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
   ) {}
 
-  public init(): void {
-    this.logger.info('Application initialized.');
-    const port = this.config.get('PORT');
-    this.logger.info(`Port number from env: ${port}`);
+  private async _initDb() {
+    const mongoUri = getMongoURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME'),
+    );
+
+    return this.databaseClient.connect(mongoUri);
+  }
+
+  public async init() {
+    this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
+    this.logger.info('Init database...');
+    await this._initDb();
+    this.logger.info('Init database completed');
   }
 }
